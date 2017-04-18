509890a24d45d1e951116dbbdf14f8515e985faa
Kyle Chivers <kchiv@Kyles-MacBook-Pro.local>
2017-04-17 19:54:25 -0700

    removing extra code

--- mapapp.js
+++ mapapp.js
@@ -1,14 +1,47 @@
 var map;
 
-// Create a new blank array for all the listing markers.
+// Create a new blank array for all the landmark markers.
 var markers = [];
-
-// This global polygon variable is to ensure only ONE polygon is rendered.
-var polygon = null;
 
 // Create placemarkers array to use in multiple functions to have control
 // over the number of places that show.
 var placeMarkers = [];
+
+// Locations array
+var locations = [
+  {title: 'Colosseum', location: {lat: 41.8902, lng: 12.4922}},
+  {title: 'Pantheon', location: {lat: 41.8986, lng: 12.4769}},
+  {title: 'Roman Forum', location: {lat: 41.8925, lng: 12.4853}},
+  {title: 'Trevi Fountain', location: {lat: 41.9009, lng: 12.4833}},
+  {title: 'St. Peter\'s Basilica', location: {lat: 41.9022, lng: 12.4539}},
+  {title: 'Castel Sant\'Angelo', location: {lat: 41.9031, lng: 12.4663}},
+  {title: 'Palatine Hill', location: {lat: 41.8894, lng: 12.4875}},
+  {title: 'Vatican Museums', location: {lat: 41.9065, lng: 12.4536}},
+  {title: 'Sistine Chapel', location: {lat: 41.9029, lng: 12.4545}},
+  {title: 'Piazza Navona', location: {lat: 41.8992, lng: 12.4731}},
+  {title: 'Spanish Steps', location: {lat: 41.9060, lng: 12.4828}},
+  {title: 'Capitoline Hill', location: {lat: 41.8934, lng: 12.4828}},
+  {title: 'Galleria Borghese', location: {lat: 41.9142, lng: 12.4921}},
+  {title: 'Basilica di Maggiore', location: {lat: 41.8976, lng: 12.4984}},
+  {title: 'Altare della Patria', location: {lat: 41.8946, lng: 12.4831}},
+  {title: 'Piazza di Spagna', location: {lat: 41.9057, lng: 12.4823}},
+  {title: 'Bocca della Verità', location: {lat: 41.8881, lng: 12.4814}},
+  {title: 'Arch of Constantine', location: {lat: 41.8898, lng: 12.4906}},
+  {title: 'Circus Maximus', location: {lat: 41.8861, lng: 12.4851}},
+  {title: 'Aventine Hill', location: {lat: 41.8825, lng: 12.4776}},
+  {title: 'Campo de\' Fiori', location: {lat: 41.8956, lng: 12.4722}},
+  {title: 'Ponte Sisto', location: {lat: 41.8924, lng: 12.4708}},
+  {title: 'Piazza Barberini', location: {lat: 41.9038, lng: 12.4886}},
+  {title: 'Quirinal Palace', location: {lat: 41.8996, lng: 12.4870}},
+  {title: 'Palazzo delle Esposizioni', location: {lat: 41.8995, lng: 12.4901}},
+  {title: 'Santa Cecilia in Trastevere', location: {lat: 41.8876, lng: 12.4758}},
+  {title: 'Trajan\'s Market', location: {lat: 41.8956, lng: 12.4862}},
+  {title: 'Trajan\'s Forum', location: {lat: 41.8952, lng: 12.4853}},
+  {title: 'Trajan\'s Column', location: {lat: 41.8958, lng: 12.4843}},
+  {title: 'Forum of Augustus', location: {lat: 41.8943, lng: 12.4869}},
+  {title: 'Capitoline Museums', location: {lat: 41.8929, lng: 12.4826}},
+  {title: 'Theatre of Marcellus', location: {lat: 41.8919, lng: 12.4799}}
+];
 
 function initMap() {
   // Create a styles array to use with the map.
@@ -81,50 +114,13 @@ function initMap() {
 
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
+    center: {lat: 41.8992, lng: 12.4731},
     zoom: 13,
     styles: styles,
     mapTypeControl: false
   });
 
-  // This autocomplete is for use in the search within time entry box.
-  var timeAutocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('search-within-time-text'));
-  // This autocomplete is for use in the geocoder entry box.
-  var zoomAutocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('zoom-to-area-text'));
-  // Bias the boundaries within the map for the zoom to area text.
-  zoomAutocomplete.bindTo('bounds', map);
-  // Create a searchbox in order to execute a places search
-  var searchBox = new google.maps.places.SearchBox(
-      document.getElementById('places-search'));
-  // Bias the searchbox to within the bounds of the map.
-  searchBox.setBounds(map.getBounds());
-
-  // These are the real estate listings that will be shown to the user.
-  // Normally we'd have these in a database instead.
-  var locations = [
-    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-  ];
-
   var largeInfowindow = new google.maps.InfoWindow();
-
-  // Initialize the drawing manager.
-  var drawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.POLYGON,
-    drawingControl: true,
-    drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_LEFT,
-      drawingModes: [
-        google.maps.drawing.OverlayType.POLYGON
-      ]
-    }
-  });
 
   // Style the markers a bit. This will be our listing marker icon.
   var defaultIcon = makeMarkerIcon('0091ff');
@@ -167,49 +163,6 @@ function initMap() {
     hideMarkers(markers);
   });
 
-  document.getElementById('toggle-drawing').addEventListener('click', function() {
-    toggleDrawing(drawingManager);
-  });
-
-  document.getElementById('zoom-to-area').addEventListener('click', function() {
-    zoomToArea();
-  });
-
-  document.getElementById('search-within-time').addEventListener('click', function() {
-    searchWithinTime();
-  });
-
-  // Listen for the event fired when the user selects a prediction from the
-  // picklist and retrieve more details for that place.
-  searchBox.addListener('places_changed', function() {
-    searchBoxPlaces(this);
-  });
-
-  // Listen for the event fired when the user selects a prediction and clicks
-  // "go" more details for that place.
-  document.getElementById('go-places').addEventListener('click', textSearchPlaces);
-
-  // Add an event listener so that the polygon is captured,  call the
-  // searchWithinPolygon function. This will show the markers in the polygon,
-  // and hide any outside of it.
-  drawingManager.addListener('overlaycomplete', function(event) {
-    // First, check if there is an existing polygon.
-    // If there is, get rid of it and remove the markers
-    if (polygon) {
-      polygon.setMap(null);
-      hideMarkers(markers);
-    }
-    // Switching the drawing mode to the HAND (i.e., no longer drawing).
-    drawingManager.setDrawingMode(null);
-    // Creating a new editable polygon from the overlay.
-    polygon = event.overlay;
-    polygon.setEditable(true);
-    // Searching within the polygon.
-    searchWithinPolygon(polygon);
-    // Make sure the search is re-done if the poly is changed.
-    polygon.getPath().addListener('set_at', searchWithinPolygon);
-    polygon.getPath().addListener('insert_at', searchWithinPolygon);
-  });
 }
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
@@ -288,253 +241,6 @@ function makeMarkerIcon(markerColor) {
     new google.maps.Point(10, 34),
     new google.maps.Size(21,34));
   return markerImage;
-}
-
-// This shows and hides (respectively) the drawing options.
-function toggleDrawing(drawingManager) {
-  if (drawingManager.map) {
-    drawingManager.setMap(null);
-    // In case the user drew anything, get rid of the polygon
-    if (polygon !== null) {
-      polygon.setMap(null);
-    }
-  } else {
-    drawingManager.setMap(map);
-  }
-}
-
-// This function hides all markers outside the polygon,
-// and shows only the ones within it. This is so that the
-// user can specify an exact area of search.
-function searchWithinPolygon() {
-  for (var i = 0; i < markers.length; i++) {
-    if (google.maps.geometry.poly.containsLocation(markers[i].position, polygon)) {
-      markers[i].setMap(map);
-    } else {
-      markers[i].setMap(null);
-    }
-  }
-}
-
-// This function takes the input value in the find nearby area text input
-// locates it, and then zooms into that area. This is so that the user can
-// show all listings, then decide to focus on one area of the map.
-function zoomToArea() {
-  // Initialize the geocoder.
-  var geocoder = new google.maps.Geocoder();
-  // Get the address or place that the user entered.
-  var address = document.getElementById('zoom-to-area-text').value;
-  // Make sure the address isn't blank.
-  if (address == '') {
-    window.alert('You must enter an area, or address.');
-  } else {
-    // Geocode the address/area entered to get the center. Then, center the map
-    // on it and zoom in
-    geocoder.geocode(
-      { address: address,
-        componentRestrictions: {locality: 'New York'}
-      }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          map.setZoom(15);
-        } else {
-          window.alert('We could not find that location - try entering a more' +
-              ' specific place.');
-        }
-      });
-    }
-  }
-
-// This function allows the user to input a desired travel time, in
-// minutes, and a travel mode, and a location - and only show the listings
-// that are within that travel time (via that travel mode) of the location
-function searchWithinTime() {
-  // Initialize the distance matrix service.
-  var distanceMatrixService = new google.maps.DistanceMatrixService;
-  var address = document.getElementById('search-within-time-text').value;
-  // Check to make sure the place entered isn't blank.
-  if (address == '') {
-    window.alert('You must enter an address.');
-  } else {
-    hideMarkers(markers);
-    // Use the distance matrix service to calculate the duration of the
-    // routes between all our markers, and the destination address entered
-    // by the user. Then put all the origins into an origin matrix.
-    var origins = [];
-    for (var i = 0; i < markers.length; i++) {
-      origins[i] = markers[i].position;
-    }
-    var destination = address;
-    var mode = document.getElementById('mode').value;
-    // Now that both the origins and destination are defined, get all the
-    // info for the distances between them.
-    distanceMatrixService.getDistanceMatrix({
-      origins: origins,
-      destinations: [destination],
-      travelMode: google.maps.TravelMode[mode],
-      unitSystem: google.maps.UnitSystem.IMPERIAL,
-    }, function(response, status) {
-      if (status !== google.maps.DistanceMatrixStatus.OK) {
-        window.alert('Error was: ' + status);
-      } else {
-        displayMarkersWithinTime(response);
-      }
-    });
-  }
-}
-
-// This function will go through each of the results, and,
-// if the distance is LESS than the value in the picker, show it on the map.
-function displayMarkersWithinTime(response) {
-  var maxDuration = document.getElementById('max-duration').value;
-  var origins = response.originAddresses;
-  var destinations = response.destinationAddresses;
-  // Parse through the results, and get the distance and duration of each.
-  // Because there might be  multiple origins and destinations we have a nested loop
-  // Then, make sure at least 1 result was found.
-  var atLeastOne = false;
-  for (var i = 0; i < origins.length; i++) {
-    var results = response.rows[i].elements;
-    for (var j = 0; j < results.length; j++) {
-      var element = results[j];
-      if (element.status === "OK") {
-        // The distance is returned in feet, but the TEXT is in miles. If we wanted to switch
-        // the function to show markers within a user-entered DISTANCE, we would need the
-        // value for distance, but for now we only need the text.
-        var distanceText = element.distance.text;
-        // Duration value is given in seconds so we make it MINUTES. We need both the value
-        // and the text.
-        var duration = element.duration.value / 60;
-        var durationText = element.duration.text;
-        if (duration <= maxDuration) {
-          //the origin [i] should = the markers[i]
-          markers[i].setMap(map);
-          atLeastOne = true;
-          // Create a mini infowindow to open immediately and contain the
-          // distance and duration
-          var infowindow = new google.maps.InfoWindow({
-            content: durationText + ' away, ' + distanceText +
-              '<div><input type=\"button\" value=\"View Route\" onclick =' +
-              '\"displayDirections(&quot;' + origins[i] + '&quot;);\"></input></div>'
-          });
-          infowindow.open(map, markers[i]);
-          // Put this in so that this small window closes if the user clicks
-          // the marker, when the big infowindow opens
-          markers[i].infowindow = infowindow;
-          google.maps.event.addListener(markers[i], 'click', function() {
-            this.infowindow.close();
-          });
-        }
-      }
-    }
-  }
-  if (!atLeastOne) {
-    window.alert('We could not find any locations within that distance!');
-  }
-}
-
-// This function is in response to the user selecting "show route" on one
-// of the markers within the calculated distance. This will display the route
-// on the map.
-function displayDirections(origin) {
-  hideMarkers(markers);
-  var directionsService = new google.maps.DirectionsService;
-  // Get the destination address from the user entered value.
-  var destinationAddress =
-      document.getElementById('search-within-time-text').value;
-  // Get mode again from the user entered value.
-  var mode = document.getElementById('mode').value;
-  directionsService.route({
-    // The origin is the passed in marker's position.
-    origin: origin,
-    // The destination is user entered address.
-    destination: destinationAddress,
-    travelMode: google.maps.TravelMode[mode]
-  }, function(response, status) {
-    if (status === google.maps.DirectionsStatus.OK) {
-      var directionsDisplay = new google.maps.DirectionsRenderer({
-        map: map,
-        directions: response,
-        draggable: true,
-        polylineOptions: {
-          strokeColor: 'green'
-        }
-      });
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-}
-
-// This function fires when the user selects a searchbox picklist item.
-// It will do a nearby search using the selected query string or place.
-function searchBoxPlaces(searchBox) {
-  hideMarkers(placeMarkers);
-  var places = searchBox.getPlaces();
-  if (places.length == 0) {
-    window.alert('We did not find any places matching that search!');
-  } else {
-  // For each place, get the icon, name and location.
-    createMarkersForPlaces(places);
-  }
-}
-
-// This function firest when the user select "go" on the places search.
-// It will do a nearby search using the entered query string or place.
-function textSearchPlaces() {
-  var bounds = map.getBounds();
-  hideMarkers(placeMarkers);
-  var placesService = new google.maps.places.PlacesService(map);
-  placesService.textSearch({
-    query: document.getElementById('places-search').value,
-    bounds: bounds
-  }, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      createMarkersForPlaces(results);
-    }
-  });
-}
-
-// This function creates markers for each place found in either places search.
-function createMarkersForPlaces(places) {
-  var bounds = new google.maps.LatLngBounds();
-  for (var i = 0; i < places.length; i++) {
-    var place = places[i];
-    var icon = {
-      url: place.icon,
-      size: new google.maps.Size(35, 35),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(15, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    };
-    // Create a marker for each place.
-    var marker = new google.maps.Marker({
-      map: map,
-      icon: icon,
-      title: place.name,
-      position: place.geometry.location,
-      id: place.place_id
-    });
-    // Create a single infowindow to be used with the place details information
-    // so that only one is open at once.
-    var placeInfoWindow = new google.maps.InfoWindow();
-    // If a marker is clicked, do a place details search on it in the next function.
-    marker.addListener('click', function() {
-      if (placeInfoWindow.marker == this) {
-        console.log("This infowindow already is on this marker!");
-      } else {
-        getPlacesDetails(this, placeInfoWindow);
-      }
-    });
-    placeMarkers.push(marker);
-    if (place.geometry.viewport) {
-      // Only geocodes have viewport.
-      bounds.union(place.geometry.viewport);
-    } else {
-      bounds.extend(place.geometry.location);
-    }
-  }
-  map.fitBounds(bounds);
 }
 
 // This is the PLACE DETAILS search - it's the most detailed so it's only
